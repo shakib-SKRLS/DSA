b080869eb22890a09c7242e4660e4158d90a222d
shakib-SKRLS <sk2724274@gmail.com>
2026-09-02 16:32:06 +0530

    solved some more question in pattern

--- pattern/program.cpp
+++ pattern/program.cpp
@@ -32,28 +32,133 @@ using namespace std;
 // }
 
 
-void print3(int n){
-    for(int i=0; i<n; i++){
-        // space
-        for(int j=0; j<i; j++){
-            cout << " ";
-        }
-        // stars
-        for(int j=0; j<2*n-(2*i+1); j++){
+// void print3(int n){
+//     for(int i=0; i<n; i++){
+//         // space
+//         for(int j=0; j<i; j++){
+//             cout << " ";
+//         }
+//         // stars
+//         for(int j=0; j<2*n-(2*i+1); j++){
+//             cout << "*";
+//         }
+//          // space
+//         for(int j=0; j<i; j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+
+// void print4(int n){
+//     for(int i=1; i<=2*n-1; i++){
+//         int stars = i;
+//         if(i>n){
+//             stars = 2*n-i;
+//         }
+//         for(int j=1; j<=stars; j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// void print5(int n){
+//     int start = 1;
+//     for(int i=0; i<n; i++){
+//         if(i%2==0) start =1;
+//         else start =0;
+//         for(int j=0; j<=i; j++){
+//             cout << start << " ";
+//             start = 1-start;
+//         }
+//         cout << endl;
+//     }
+// }
+
+// void print6(int n){
+//   int space = 2*(n-1);
+//   for(int i=1; i<=n; i++){
+//     //numbers
+//     for(int j=1; j<=i; j++){
+//         cout << j;
+//     }
+//     //spaces
+//     for(int j=1; j<=space; j++){
+//         cout << " ";
+//     }
+//     //numbers
+//     for(int j=i; j>=1; j--){
+//         cout << j;
+//     }
+//     cout << endl;
+//     space -= 2;
+//   }  
+// }
+
+// void print7(int n){
+//     for(int i=0; i<n; i++){
+//         for(char ch= 'A'; ch<='A'+i; ch++){
+//             cout << ch << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// void print8(int n){
+//     for(int i=0; i<n; i++){
+//         for(char ch='A'; ch<='A' +(n-i-1); ch++){
+//             cout << ch << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+
+// void print9(int n){
+    
+//     for(int i=0; i<n; i++){
+//         // spaces
+//         for(int j=0; j<n-i-1; j++){
+//             cout << " ";
+//         }
+//         // characters
+//         char ch ='A';
+//         int breakpoint = (2*i+1)/2;
+//         for(int j=0; j<2*i+1; j++){
+//             cout << ch;
+//             if(j<breakpoint) ch++;
+//             else ch--;
+//         }
+//         // spaces
+//         for(int j=0; j<n-i-1; j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+
+    
+// }
+
+
+void print10(int n){
+   for (int i=0; i<n; i++){
+    for(int j=0; j<n; j++){
+        if(i==0 || j==0 || i==n-1 || j==n-1){
             cout << "*";
         }
-         // space
-        for(int j=0; j<i; j++){
+        else{
             cout << " ";
         }
-        cout << endl;
     }
+    cout << endl;
+   }
+    
 }
 
-
 int main(){
-    int n = 5;
-    print3(n);
+    int n = 4;
+    print10(n);
 
     return 0;
 }

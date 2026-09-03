4f6206133d91c9be21f9fd3a6919ecc12199d8f2
shakib-SKRLS <sk2724274@gmail.com>
2026-09-03 14:56:54 +0530

    practice one more question in pattern

--- pattern/program.cpp
+++ pattern/program.cpp
@@ -141,24 +141,38 @@ using namespace std;
 // }
 
 
-void print10(int n){
-   for (int i=0; i<n; i++){
-    for(int j=0; j<n; j++){
-        if(i==0 || j==0 || i==n-1 || j==n-1){
-            cout << "*";
-        }
-        else{
-            cout << " ";
-        }
-    }
-    cout << endl;
-   }
+// void print10(int n){
+//    for (int i=0; i<n; i++){
+//     for(int j=0; j<n; j++){
+//         if(i==0 || j==0 || i==n-1 || j==n-1){
+//             cout << "*";
+//         }
+//         else{
+//             cout << " ";
+//         }
+//     }
+//     cout << endl;
+//    }
     
+// }
+
+void print11(int n){
+    for(int i=0; i<2*n-1; i++){
+        for(int j=0; j<2*n-1; j++){
+           int top = i;
+           int left = j;
+           int  bottom = 2*n-2-i;
+           int right = 2*n-2-j;
+           int minDist = min(min(top, bottom), min(left, right));
+           cout << n - minDist<< " ";
+        }
+        cout << endl;
+    }
 }
 
 int main(){
     int n = 4;
-    print10(n);
+    print11(n);
 
     return 0;
 }

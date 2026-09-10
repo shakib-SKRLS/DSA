67053ebcae36f9d6085336d1c2b45edf602b56e8
shakib-SKRLS <sk2724274@gmail.com>
2026-09-10 16:08:55 +0530

    pratice some more question in recursion

--- Basic Recursion/program.cpp
+++ Basic Recursion/program.cpp
@@ -50,10 +50,77 @@ using namespace std;
 // }
 
 
+// reverse an array
+void reverseArray(int arr[], int left, int right) {
+   
+    if (left >= right) {
+        return;
+    }
+
+   
+    swap(arr[left], arr[right]);
+
+    
+    reverseArray(arr, left + 1, right - 1);
+}
+
+// reverse an array with single pointer
+// void reverseArray(int arr[], int i, int n) {
+//     // Base case
+//     if (i >= n / 2) {
+//         return;
+//     }
+
+//     swap(arr[i], arr[n - i - 1]);
+
+//     reverseArray(arr, i + 1, n);
+// }
+
+
+// check if the string is palindrome or not
+// bool isPalindrome(string str, int left, int right) {
+
+//     // Base case
+//     if (left >= right) {
+//         return true;
+//     }
+
+//     // Characters don't match
+//     if (str[left] != str[right]) {
+//         return false;
+//     }
+
+//     // Move both pointers
+//     return isPalindrome(str, left + 1, right - 1);
+// }
+
+
+// febonaci
+
+int f(int n){
+    if(n<=1) return n;
+    int last=f(n-1);
+    int slast=f(n-2);
+    return last + slast;
+}
+
+
 int main(){
     
     // int mul = f(5);
     // cout<<mul;
+    // return 0;
+
+    //  int arr[] = {1, 2, 3, 4, 5};
+    // int n = 5;
+
+    // reverseArray(arr, 0, n - 1);
+
+    // for (int i = 0; i < n; i++) {
+    //     cout << arr[i] << " ";
+    // }
+    int num=f(7);
+    cout<<num;
     return 0;
 }
 

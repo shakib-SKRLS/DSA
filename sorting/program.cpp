b566f85fe786350f835cbadd1b3d4e45bbd1a52b
shakib-SKRLS <sk2724274@gmail.com>
2026-09-13 00:10:13 +0530

    bubble sort and insertion sort

--- sorting/program.cpp
+++ sorting/program.cpp
@@ -5,35 +5,83 @@ using namespace std;
 
 
 // selection sort 
-int main() {
+// int main() {
 
-    int n;
-    cin >> n;
+//     int n;
+//     cin >> n;
 
-    vector<int> arr(n);
+//     vector<int> arr(n);
 
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
 
-    // Selection Sort
-    for (int i = 0; i < n - 1; i++) {
+//     // Selection Sort
+//     for (int i = 0; i < n - 1; i++) {
 
-        int minIndex = i;
+//         int minIndex = i;
 
-        for (int j = i + 1; j < n; j++) {
+//         for (int j = i + 1; j < n; j++) {
 
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
+//             if (arr[j] < arr[minIndex]) {
+//                 minIndex = j;
+//             }
+//         }
+
+//         swap(arr[i], arr[minIndex]);
+//     }
+
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+
+//     return 0;
+// }
+
+
+// Bubble sort
+// void bubble_sort(vector<int>& arr, int n){
+//     for(int i =n-1; i>=0; i--){
+//         int didSwap=0;
+//         for(int j=0; j<=i-1; j++){
+//             if(arr[j]> arr[j+1]){
+//                 int temp=arr[j+1];
+//                 arr[j+1]=arr[j];
+//                 arr[j]=temp;
+//                 didSwap=1;
+//             }
+//         }
+//         if(didSwap==0){
+//             break;
+//         }
+//     }
+    
+// }
+
+// Insertion sort
+void insertion_sort(vector<int>& arr, int n){
+    for(int i=0; i<=n-1; i++){
+        int j=i;
+        while(j>0 && arr[j-1] > arr[j]){
+            int temp = arr[j-1];
+            arr[j-1]=arr[j];
+            arr[j]=temp;
+
+            j--;
         }
-
-        swap(arr[i], arr[minIndex]);
     }
+}
 
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
     }
-
-    return 0;
+    // bubble_sort(arr, n);
+    insertion_sort(arr, n);
+    for(auto it: arr){
+        cout<< it <<" ";
+    }
 }

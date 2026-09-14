e8fd90bad359d8115a00dfcb03daf7e4a629a55b
shakib-SKRLS <sk2724274@gmail.com>
2026-09-15 01:38:18 +0530

    quick sort

--- sorting/program.cpp
+++ sorting/program.cpp
@@ -116,13 +116,38 @@ void mS(vector<int> & arr, int left, int right){
 }
 
 
+// quick sort
 
+int partition(vector<int> &arr, int low, int high){
+    int pivot = arr[low];
+    int i=low;
+    int j=high;
+    while(i<j){
+        while(arr[i] <= pivot && i<=high-1){
+            i++;
+        }
+        while(arr[j]>pivot && j>=low+1){
+            j--;
+        }
+        if(i<j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
+}
+
+
+void qs(vector<int> &arr, int low, int high){
+    if(low<high){
+        int pIndex= partition(arr, low, high);
+        qs(arr, low, pIndex-1);
+        qs(arr, pIndex+1, high);
+}}
 
 int main(){
    
      vector<int> arr = {38, 27, 43, 3, 9, 82, 10};
 
-    mS(arr, 0, arr.size() - 1);
+    qs(arr, 0, arr.size() - 1);
 
     for (int x : arr)
         cout << x << " ";
